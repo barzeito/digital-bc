5b826ed73d645b571deb16bcea53c9c55202ac45
barzeito <barzeitony@gmail.com>
2025-04-01 21:37:54 -0700

    Added: enforce admin + auth

---
+++ backend/src/middlewares/enforce-admin.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from "express";
+import createHttpError, { Forbidden, Unauthorized } from "http-errors";
+import { ReasonPhrases } from "http-status-codes";
+import userDTO, { Roles } from "../models/auth/user-dto";
+
+declare global {
+    namespace Express {
+        export interface Request {
+            user: userDTO
+        }
+    }
+}
+
+export default function enforceAdmin(req: Request, res: Response, next: NextFunction) {
+    if (!req.user) return next(createHttpError(Unauthorized(ReasonPhrases.UNAUTHORIZED)));
+    if (req.user.roleId !== Roles.ADMIN) return next(createHttpError(Forbidden(ReasonPhrases.FORBIDDEN)));
+    return next()
+}
--- backend/src/routers/cards.ts
+++ backend/src/routers/cards.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 import validate from "../middlewares/input-validation";
 import { addCardValidator } from "../controllers/cards/validator";
 import { add, deleteCard, getAll, getOne, patch } from "../controllers/cards/controller";
+import enforceAdmin from "../middlewares/enforce-admin";
+import enforceAuth from "../middlewares/enforce-auth";
 
 const router = Router();
 
 router.get('/', getAll);
 router.get('/:company', getOne);
-router.post('/', validate(addCardValidator), add);
-router.delete('/:id', deleteCard);
-router.patch('/:id', patch);
+router.post('/', enforceAdmin, validate(addCardValidator), add);
+router.delete('/:id', enforceAdmin, deleteCard);
+router.patch('/:id', enforceAuth, patch);
 export default router;
